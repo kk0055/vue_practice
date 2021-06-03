a2cf75fda37b60b2a9b4efdbdbb6040b82f23c27
kk0055 <rtsw.lgy@gmail.com>
2021-06-03 21:15:47 +0800

    autocomlete done

--- autocomplete/src/views/About.vue
+++ autocomplete/src/views/About.vue
@@ -1,5 +1,48 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+   <input type="text" autocomplete="off" v-model="state"  @focus="modal = true" >
+   <div v-if="filteredStates && modal">
+     <ul>
+       <li v-for="filteredState in filteredStates" :key="filteredState.index" @click="setState(filteredState)">{{filteredState}}</li>
+     </ul>
+   </div>
   </div>
 </template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      state:'',
+      modal: false,
+      states: [
+        'Flodira', 'Alaska', 'texas'
+      ],
+      filteredStates:[],
+    }
+  },
+  mounted() {
+   this.filterStates();
+  },
+  methods: {
+    filterStates() {
+      if (this.state.length == 0) {
+        this.filteredStates = this.state;
+      }
+
+    this.filteredStates = this.states.filter(state => {
+      return state.toLowerCase().startsWith(this.state.toLowerCase());
+    });
+    },
+    setState(state) {
+    this.state = state;
+    this.modal = false;
+    }
+  },
+  watch: {
+    state() {
+      this.filterStates();
+    }
+  }
+}
+</script>
