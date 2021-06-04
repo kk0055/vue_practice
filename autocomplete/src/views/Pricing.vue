e3fa0a3f2c480ef1a87ba87c6a73200f1c5f2a0f
kk0055 <rtsw.lgy@gmail.com>
2021-06-04 18:10:48 +0800

    Price data

--- autocomplete/src/views/Pricing.vue
+++ autocomplete/src/views/Pricing.vue
@@ -1,31 +1,32 @@
 <template>
   <div class="flex justify-center">
-    <div class="w-1/2 bg-blue-900 rounded-lg shadow px-6 py-12 flex flex-col items-center">
-     <div class="gb-gray-400">
-       <button class="rounded-full font-bold px-6 py-1 focus:outline-none bg-blue-600 text-white" >Starter</button>
-       <button class="rounded-full font-bold px-6 py-1 focus:outline-none bg-blue-600 text-white" >Pro</button>
+    <div class="w-1/2 bg-blue-800 rounded-lg shadow px-6 py-12 flex flex-col items-center">
+     <div class="bg-gray-200 rounded-full">
+       <button @click="currentFrequency = 'monthly'" 
+       class="rounded-full font-bold px-6 py-1 focus:outline-none " 
+       :class="currentFrequency == 'monthly' ? 'bg-blue-600 text-white' : '' "
+       >Monthly</button>
+       <button  @click="currentFrequency ='yearly'" 
+       class="rounded-full font-bold px-6 py-1 focus:outline-none "
+       :class="currentFrequency == 'yearly' ? 'bg-blue-600 text-white' : '' "
+        >Yearly</button>
    </div>
    <div class="pt-8 flex w-full">
-     <div class="text-white w-1/2">
-       <h1>Starter</h1>
+     <div v-for="plan in plans" :key="plan.index" class="text-white w-1/2">
+       <h1 >Starter</h1>
        <ul>
          <li>Benefit1</li>
          <li>Benefit1</li>
          <li>Benefit1</li>
          <li>Benefit1</li>
        </ul>
+       <div class="flex justify-center pt-8">
+       <div class="text-4xl font-bold">${{plan.pricing[currentFrequency].price}}</div>
+       <div class="pl-1 pt-2">{{plan.pricing[currentFrequency].label}}</div>
+       </div>
      </div>
 
-    <div class="text-white w-1/2">
-       <h1>Starter</h1>
-       <ul>
-         <li>Benefit1</li>
-         <li>Benefit1</li>
-         <li>Benefit1</li>
-         <li>Benefit1</li>
-       </ul>
-    
-   </div>
+  
      </div>
     </div>
   </div>
@@ -33,8 +34,25 @@
 
 <script>
 export default {
-  // data: function() {
-   
-  // }
+  data: function() {
+   return {
+     currentFrequency: 'monthly',
+
+     plans: [
+       {
+          pricing: {
+            monthly: {price: 99, label: '/mo'},
+            yearly: {price: 499, label: '/yr'},
+          }
+       },
+        {
+          pricing: {
+            monthly: {price: 59, label: '/mo'},
+            yearly: {price: 899, label: '/yr'},
+          }
+       },
+     ]
+   }
+  }
 }
 </script>
