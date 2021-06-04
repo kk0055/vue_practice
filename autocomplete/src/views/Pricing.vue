de7da84a407431e424d7f3a7c2a371126f2fb588
kk0055 <rtsw.lgy@gmail.com>
2021-06-04 17:12:41 +0800

    Tailwind design

--- autocomplete/src/views/Pricing.vue
+++ autocomplete/src/views/Pricing.vue
@@ -2,9 +2,30 @@
   <div class="flex justify-center">
     <div class="w-1/2 bg-blue-900 rounded-lg shadow px-6 py-12 flex flex-col items-center">
      <div class="gb-gray-400">
-       <button class="bg-blue-600 text-white" >Starter</button>
-       <button class="bg-blue-600 text-white" >Pro</button>
-   
+       <button class="rounded-full font-bold px-6 py-1 focus:outline-none bg-blue-600 text-white" >Starter</button>
+       <button class="rounded-full font-bold px-6 py-1 focus:outline-none bg-blue-600 text-white" >Pro</button>
+   </div>
+   <div class="pt-8 flex w-full">
+     <div class="text-white w-1/2">
+       <h1>Starter</h1>
+       <ul>
+         <li>Benefit1</li>
+         <li>Benefit1</li>
+         <li>Benefit1</li>
+         <li>Benefit1</li>
+       </ul>
+     </div>
+
+    <div class="text-white w-1/2">
+       <h1>Starter</h1>
+       <ul>
+         <li>Benefit1</li>
+         <li>Benefit1</li>
+         <li>Benefit1</li>
+         <li>Benefit1</li>
+       </ul>
+    
+   </div>
      </div>
     </div>
   </div>
