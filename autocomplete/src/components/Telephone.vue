894cac3c7bc47bcc30f6ea5b78eef4696fb206bb
kk0055 <rtsw.lgy@gmail.com>
2021-06-07 16:47:01 +0800

    Telephone vue HTML

---
+++ autocomplete/src/components/Telephone.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="mt-6">
+ <input type="text" placeholder="(XXX) XXXX XXXX" class="w-36 text-2xl bg-gray-300 p-3 rounded-lg focus:outline-none">
+  </div>
+</template>
+
+<script>
+export default {
+ 
+}
+</script>
