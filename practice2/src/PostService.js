23a40403b31f86ddace0969d1a7c8264ce2a8693
kk0055 <rtsw.lgy@gmail.com>
2021-06-08 15:31:24 +0800

    axios CRUD methods

--- practice2/src/PostService.js
+++ practice2/src/PostService.js
@@ -1,9 +1,24 @@
 import axios from 'axios';
 
-const  apiBaseUrl = 'https://ndb99xkpdk.execute-api.eu-west-2.amazonaws.com/dev';
+axios.defaults.baseURL= 'https://ndb99xkpdk.execute-api.eu-west-2.amazonaws.com/dev';
 
 export default class PostService {
   getAllPosts(){
-    return axios.get(`${apiBaseUrl}/posts`);
+    return axios.get('/posts');
+  }
+
+  getPosts(number) {
+    return axios.get(`/posts/${number}`);
+}
+  // Postの投稿
+  writePost(post) {
+    if(post.id){
+      return axios.put(`/post/${ppost.id}` , post)
+      //Postが存在しない場合新規作成
+    }else return axios.post('/post', post)
+  }
+
+  deletePost(id){
+    return axios.delete(`/post/${id}`);
   }
 }
--- practice2/src/components/PostForm.vue
+++ practice2/src/components/PostForm.vue
@@ -1,10 +1,52 @@
 <template>
-  
+<form v-if="!loading" class="form" v-on:submit.prevent="onSubmit">
+  <div class="input-field">
+      <label for="title">Title</label>
+      <input type="text" name="title"
+      v-model="title"
+      class="validate"
+      >
+      <span class="helper-text" data-error="Title must not be empty" > </span>
+     
+  </div>
+
+  <div class="input-field">
+          <label for="body">Body</label>
+          <input type="text" name="body"
+          v-model="body"
+          class="validate"
+          >
+          <span class="helper-text" data-error="Body must not be empty" > </span>
+         
+  </div>
+ <button type="submit" class="waves-effect waves-light btn">
+   Add
+ </button>
+</form>
 </template>
 
 
 <script>
 export default {
-  
+  name:"PostForm",
+  data() {
+    return {
+      loading : false,
+      title: '',
+      body: ''
+    }
+  },
+  methods: {
+    onsubmit() {
+      this.loading = true;
+      
+    }
+  }
 }
 </script>
+
+<style scoped>
+  .form {
+    margin:50px; 
+  }
+</style>
