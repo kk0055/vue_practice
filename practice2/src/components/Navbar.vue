a62fc3a46519f75fb060951cd33497da07363473
kk0055 <rtsw.lgy@gmail.com>
2021-06-08 11:04:48 +0800

    materilalize installed

---
+++ practice2/src/components/Navbar.vue
@@ -0,0 +1,13 @@
+<template>
+    <nav>
+    <div class="nav-wrapper">
+      <a href="#" class="brand-logo">Vue Practice2</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="sass.html">Sass</a></li>
+        <li><a href="">Home</a></li>
+        <li><a href="">About</a></li>
+      </ul>
+    </div>
+  </nav>
+        
+</template>
