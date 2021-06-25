4af150a027c57fc3ad11b64f8bcd34d0c48c06d0
kk0055 <rtsw.lgy@gmail.com>
2021-06-25 12:30:00 +0800

    addTask

--- vue_todo/src/App.vue
+++ vue_todo/src/App.vue
@@ -1,21 +1,34 @@
 <template>
  <div class="container">
   <Header title="Task Tracker"/>  
+  <AddTask @add-task="addTask"/>
  </div>
 </template>
 
 <script>
 import Header from './components/Header'
+import AddTask from './components/AddTask'
 
 export default {
   name: 'App',
   components: {
-    Header
+    Header,
+    AddTask
   },
   data() {
     return {
     tasks: []
     }
+  },
+  methods: {
+   addTask(task) {
+     this.tasks = [...tasks, task]
+   },
+   deleteTask(id) {
+     if (confirm('Are you sure?')){
+       this.tasks = this.tasks.filter((task) => task.id !== id)
+     }
+   }
   },
   created() {
     this.tasks = ''
