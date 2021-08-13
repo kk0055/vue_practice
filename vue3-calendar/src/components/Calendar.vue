3b31accad55448543257311c8a2e5cc493c1417f
kk0055 <rtsw.lgy@gmail.com>
2021-08-13 11:17:10 +0800

    ブラウザに出力

--- vue3-calendar/src/components/Calendar.vue
+++ vue3-calendar/src/components/Calendar.vue
@@ -1,6 +1,28 @@
 <template>
-    <p>Hello World</p>
+  <h2>カレンダー{{ currentDate }}</h2>
+  <button @click="prevMonth">前の月</button>
+  <button @click="nextMonth">次の月</button>
+<div style="max-width:900px;border-top:1px solid gray;">
+  <div
+    v-for="(week, index) in calendars"
+    :key="index"
+    style="display:flex;border-left:1px solid gray"
+  >
+    <div
+      v-for="(day, index) in week"
+      :key="index"
+      style="
+          flex:1;min-height:125px;
+          border-right:1px solid gray;
+          border-bottom:1px solid gray
+        "
+    >
+      {{ day.date }}
+    </div>
+  </div>
+</div>
 </template>
+
 <script>
 import moment from "moment";
 export default {
@@ -21,13 +43,33 @@ export default {
       date.endOf("month");
       const youbiNum = date.day();
       return date.add(6 - youbiNum, "days");
-    }
+    },
+    getCalendar() {
+      const startDate = this.getStartDate();
+      const endDate = this.getEndDate();
+      const weekNumber = Math.ceil(endDate.diff(startDate, "days") / 7);
+
+      let calendars = [];
+      for (let week = 0; week < weekNumber; week++) {
+        let weekRow = [];
+        for (let day = 0; day < 7; day++) {
+          weekRow.push({
+            date: startDate.get("date"),
+          });
+          startDate.add(1, "days");
+        }
+        calendars.push(weekRow);
+      }
+      return calendars;
+    },
   },
-   mounted(){
-  const startDate = this.getStartDate();
-  const endDate = this.getEndDate();
-  const weekNumber = Math.ceil(endDate.diff(startDate, "days") / 7);
-  console.log(weekNumber)
-  } 
-}
+  mounted() {
+  console.log(this.getCalendar());
+  },  
+  computed: {
+    calendars() {
+      return this.getCalendar();
+    },
+  },
+};
 </script>
