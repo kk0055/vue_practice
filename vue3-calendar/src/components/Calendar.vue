40f1959052c937da1c3522a9590a3950c2c8f540
kk0055 <rtsw.lgy@gmail.com>
2021-08-13 10:54:00 +0800

    週の数を取得

---
+++ vue3-calendar/src/components/Calendar.vue
@@ -0,0 +1,33 @@
+<template>
+    <p>Hello World</p>
+</template>
+<script>
+import moment from "moment";
+export default {
+  data() {
+    return {
+      currentDate: moment(),
+    };
+  },
+  methods: {
+    getStartDate() {
+      let date = moment(this.currentDate);
+      date.startOf("month");
+      const youbiNum = date.day();
+      return date.subtract(youbiNum, "days");
+    },
+    getEndDate() {
+      let date = moment(this.currentDate);
+      date.endOf("month");
+      const youbiNum = date.day();
+      return date.add(6 - youbiNum, "days");
+    }
+  },
+   mounted(){
+  const startDate = this.getStartDate();
+  const endDate = this.getEndDate();
+  const weekNumber = Math.ceil(endDate.diff(startDate, "days") / 7);
+  console.log(weekNumber)
+  } 
+}
+</script>
