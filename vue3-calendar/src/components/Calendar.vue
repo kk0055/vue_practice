446c08e7ce29c953309440e2644b4b4a402e0511
kk0055 <rtsw.lgy@gmail.com>
2021-08-13 13:23:56 +0800

    曜日の出力

--- vue3-calendar/src/components/Calendar.vue
+++ vue3-calendar/src/components/Calendar.vue
@@ -1,26 +1,34 @@
 <template>
-  <h2>カレンダー{{ currentDate }}</h2>
-  <button @click="prevMonth">前の月</button>
-  <button @click="nextMonth">次の月</button>
-<div style="max-width:900px;border-top:1px solid gray;">
-  <div
-    v-for="(week, index) in calendars"
-    :key="index"
-    style="display:flex;border-left:1px solid gray"
-  >
-    <div
-      v-for="(day, index) in week"
-      :key="index"
-      style="
-          flex:1;min-height:125px;
-          border-right:1px solid gray;
-          border-bottom:1px solid gray
-        "
-    >
-      {{ day.date }}
+  <div class="content">
+   <h2>カレンダー {{ displayMonth }}</h2>
+    <div class="button-area">
+      <button @click="prevMonth" class="button">前の月</button>
+      <button @click="nextMonth" class="button">次の月</button>
+    </div>
+    <div class="calendar">
+        <div class="calendar-weekly">
+    <div class="calendar-youbi" v-for="n in 7" :key="n">
+      {{ dayOfWeek(n-1) }}
     </div>
   </div>
-</div>
+      <div 
+        class="calendar-weekly"
+        v-for="(week, index) in calendars"
+        :key="index"
+      >
+        <div
+          class="calendar-daily"
+          :class="{outside: currentMonth !== day.month}"
+          v-for="(day, index) in week"
+          :key="index"
+        >
+          <div class="calendar-day">
+            {{ day.day }}
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -50,13 +58,16 @@ export default {
       const weekNumber = Math.ceil(endDate.diff(startDate, "days") / 7);
 
       let calendars = [];
+      let calendarDate = this.getStartDate();
+
       for (let week = 0; week < weekNumber; week++) {
         let weekRow = [];
         for (let day = 0; day < 7; day++) {
           weekRow.push({
-            date: startDate.get("date"),
+          day: calendarDate.get("date"),
+          month: calendarDate.format("YYYY-MM"),
           });
-          startDate.add(1, "days");
+          calendarDate.add(1, "days");
         }
         calendars.push(weekRow);
       }
@@ -68,6 +79,10 @@ export default {
     prevMonth() {
     this.currentDate = moment(this.currentDate).subtract(1, "month");
 },
+dayOfWeek(dayIndex) {
+  const week = ["日", "月", "火", "水", "木", "金", "土"];
+  return week[dayIndex];
+},
   },
   mounted() {
   console.log(this.getCalendar());
@@ -76,6 +91,55 @@ export default {
     calendars() {
       return this.getCalendar();
     },
+     displayMonth(){
+    return this.currentDate.format('YYYY[年]M[月]')
+  },
+  currentMonth(){
+  return this.currentDate.format('YYYY-MM')
+},
   },
 };
 </script>
+
+<style>
+.content{
+  margin:2em auto;
+  width:900px;
+}
+.button-area{
+  margin:0.5em 0;
+}
+.button{
+  padding:4px 8px;
+  margin-right:8px;
+}
+.calendar{
+  max-width:900px;
+  border-top:1px solid #E0E0E0;
+  font-size:0.8em;
+}
+.calendar-weekly{
+  display:flex;
+  border-left:1px solid #E0E0E0;
+  /* background-color: black; */
+}
+.calendar-daily{
+  flex:1;
+  min-height:125px;
+  border-right:1px solid #E0E0E0;
+  border-bottom:1px solid #E0E0E0;
+  margin-right:-1px;
+}
+.calendar-day{
+  text-align: center;
+}
+.calendar-youbi{
+  flex:1;
+  border-right:1px solid #E0E0E0;
+  margin-right:-1px;
+  text-align:center;
+}
+.outside{
+  background-color: #f7f7f7;
+}
+</style>
