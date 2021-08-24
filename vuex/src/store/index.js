a0e941426864a448b65aea45091c7a6d2a32c2c9
kk0055 <rtsw.lgy@gmail.com>
2021-08-24 11:44:07 +0800

    vuex actions

--- vuex/src/store/index.js
+++ vuex/src/store/index.js
@@ -1,18 +1,26 @@
 import { createStore } from 'vuex'
+import axios from 'axios'
 
 export default createStore({
   state: {
     counter:0
   },
   mutations: {
-    increaseCounter(state) {
-      state.counter++
-    },
-     decreaseCounter(state) {
+    decreaseCounter(state) {
       state.counter--
     },
+    increaseCounter(state, payload) {
+      state.counter += payload
+    },
+ 
   },
   actions: {
+    increaseCounter({commit}) {
+      axios('https://www.random.org/integers/?num=1&min=1&max=6&col=1&base=10&format=plain&rnd=new')
+      .then(response =>  {
+        commit('increaseCounter',response.data)
+      })
+    },
   },
   modules: {
   }
