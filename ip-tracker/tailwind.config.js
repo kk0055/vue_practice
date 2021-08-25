d485d9d6f0d2f920283da9381457a76205e68874
kk0055 <rtsw.lgy@gmail.com>
2021-08-25 18:34:34 +0800

    Tailwind route setting

--- ip-tracker/tailwind.config.js
+++ ip-tracker/tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: { content: ['./public/**/*.html', './src/**/*.vue'] },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: (theme) => ({
+        "hero-pattern": "url('https://images.pexels.com/photos/2806489/pexels-photo-2806489.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260')",
+      }),
+    },
   },
   variants: {
     extend: {},
