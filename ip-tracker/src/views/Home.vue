0802e7885e9f129160b4de9028af4ad8382dd16d
kk0055 <rtsw.lgy@gmail.com>
2021-08-26 13:33:39 +0800

    leaflet Map setting

--- ip-tracker/src/views/Home.vue
+++ ip-tracker/src/views/Home.vue
@@ -41,11 +41,62 @@
 <script>
 // @ is an alias to /src
 import IPInfo from "../components/IPInfo.vue";
+import leaflet from "leaflet";
+import { onMounted, ref } from "vue";
 
 export default {
   name: 'Home',
   components: {
   IPInfo 
-  }
-}
+  },
+    setup() {
+    // create map variable
+    let mymap;
+    // data
+    const queryIp = ref("");
+    const ipInfo = ref(null);
+    // mounted lifecycle hook, creates the map
+    onMounted(() => {
+      mymap = leaflet.map("mapid").setView([42.5145, -83.0147], 9);
+      leaflet
+        .tileLayer(
+          "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoia2swMDU1IiwiYSI6ImNrc3NnNTQyYzA0MGsydm8zNTNvOGo3M2oifQ.Nrm9JIUbDm_vejkvaSIfsw",
+          {
+            attribution:
+              'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+            maxZoom: 18,
+            id: "mapbox/streets-v11",
+            tileSize: 512,
+            zoomOffset: -1,
+            accessToken:
+              "pk.eyJ1Ijoia2swMDU1IiwiYSI6ImNrc3NnNTQyYzA0MGsydm8zNTNvOGo3M2oifQ.Nrm9JIUbDm_vejkvaSIfsw",
+          }
+        )
+        .addTo(mymap);
+    });
+    // gets ip information from API
+    const getIpInfo = async () => {
+      try {
+        const data = await axios.get(
+          `https://geo.ipify.org/api/v1?apiKey=at_FBTi0rGPhQlKp2cRdOJV0t3Mibq6F&ipAddress=${queryIp.value}`
+        );
+        const result = data.data;
+        ipInfo.value = {
+          address: result.ip,
+          state: result.location.region,
+          timezone: result.location.timezone,
+          isp: result.isp,
+          lat: result.location.lat,
+          lng: result.location.lng,
+        };
+        leaflet.marker([ipInfo.value.lat, ipInfo.value.lng]).addTo(mymap);
+        mymap.setView([ipInfo.value.lat, ipInfo.value.lng], 13);
+      } catch (err) {
+        alert(err.message);
+      }
+    };
+    return { queryIp, ipInfo, getIpInfo };
+  },
+};
 </script>
+
