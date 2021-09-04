abf00877f84b605db5107e4affda09df0b554ea4
kk0055 <rtsw.lgy@gmail.com>
2021-09-04 17:29:55 +0800

    Results.vue修正

--- reaction-time/src/App.vue
+++ reaction-time/src/App.vue
@@ -46,4 +46,19 @@ export default {
   margin-top:60px;
   color: #444;
 }
+button {
+  background: #0faf87;
+  color: white;
+  border:none;
+  padding: 8px 16px;
+  border-radius: 4px;
+  font-size: 16px;
+  letter-spacing: 1px;
+  cursor: pointer;
+  margin:10px;
+}
+button[disabled] {
+opacity: 0.2;
+cursor: not-allowed;
+}
 </style>
--- reaction-time/src/components/Result.vue
+++ reaction-time/src/components/Result.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="block">
       <p  >Reaction Time: {{score}}</p>
+      <p class="rank">{{rank}}</p>
   </div>
 </template>
 
@@ -8,10 +9,28 @@
 export default {
   props:[
     'score'
-  ]
+  ],
+  data() {
+   return {
+     rank:null
+   }
+  },
+  mounted() {
+    if (this.score < 250) {
+      this.rank = 'Super man'
+    }else if (this.score < 400){
+      this.rank = 'まあまあ'
+     }else {
+       this.rank = 'Super slow....'
+     }
+  }
 }
 </script>
 
 <style >
- 
+ .rank{
+  font-size: 1.4em;
+  color: blue;
+  font-weight: bold;
+ }
 </style>
