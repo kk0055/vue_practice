e5b196097d2f77eb11475a14fdabbcdfcbe77e0c
kk0055 <rtsw.lgy@gmail.com>
2021-09-13 17:11:21 +0800

    Navigation作成

--- invoice-app/src/App.vue
+++ invoice-app/src/App.vue
@@ -1,7 +1,44 @@
 <template>
-  <router-view />
+ <div>
+   <div v-if="!mobile" class="app flex">
+     <Navigation/>
+      <div class="app flex flex-column">
+      <router-view />
+   </div>
+   </div>
+       <div v-else class="mobile-message flex flex-column">
+      <h2>Sorry, this app is not supported on Mobile Devices</h2>
+      <p>To use this app, please use a computer or Tablet</p>
+    </div>
+ </div>
 </template>
-
+<script>
+import Navigation from "./components/Navigation";
+export default {
+  data() {
+   return {
+     mobile:null,
+   }
+  },
+  components: {
+Navigation
+  },
+    created() {
+   
+    // this.checkScreen();
+  },
+  methods: {
+    checkScreen() {
+  const windowWidth = window.innerWidth;
+  if (windowWidth <= 750) {
+    this.mobile = true;
+    return;
+  }
+  this.mobile = false;
+},
+  }
+}
+</script>
 <style lang="scss">
 @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap");
 * {
@@ -9,7 +46,38 @@
   padding: 0;
   box-sizing: border-box;
   font-family: "Poppins", sans-serif;
+}
+.app {
   background-color: #141625;
+  min-height: 100vh;
+  @media (min-width: 900px) {
+    flex-direction: row !important;
+  }
+  .app-content {
+    padding: 0 20px;
+    flex: 1;
+    position: relative;
+  }
+}
+.mobile-message {
+  text-align: center;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: #141625;
+  color: #fff;
+  p {
+    margin-top: 16px;
+  }
+}
+// animated invoice
+.invoice-enter-active,
+.invoice-leave-active {
+  transition: 0.8s ease all;
+}
+.invoice-enter-from,
+.invoice-leave-to {
+  transform: translateX(-700px);
 }
 button,
 .button {
