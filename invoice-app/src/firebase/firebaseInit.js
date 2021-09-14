0684701e0575da1acba8964e35e0f2ddbb080825
kk0055 <rtsw.lgy@gmail.com>
2021-09-14 15:55:11 +0800

    firebase エラー修正　データ登録完了

--- invoice-app/src/firebase/firebaseInit.js
+++ invoice-app/src/firebase/firebaseInit.js
@@ -1,7 +1,6 @@
-import { initializeApp } from "firebase/app";
-import "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -14,4 +13,6 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+export default firebaseApp.firestore();
