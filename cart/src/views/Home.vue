cee56d569be15dc4f4dcdbcb4656c1e47f883b4c
kk0055 <rtsw.lgy@gmail.com>
2021-10-19 13:34:56 +0800

    Productcardにコンポーネント化

--- cart/src/views/Home.vue
+++ cart/src/views/Home.vue
@@ -13,46 +13,15 @@
 
         <div class="recommended">
 
-          <div v-for="(product, i) in inventory.slice(0,3)" 
-          :key="product.id" class="card">
-            <div class="card-title">
-              {{ product.name }}
-            </div>
-            <div class="card-body">
-              <i class="icofont-10x icofont-{{ product.icon }}"></i>
-              <form>
-                <div class="row">
-                  <div class="cell">
-                    <label>Type:</label>
-                  </div>
-                  <div class="cell">
-                    <em>{{ product.type }}</em>
-                  </div>
-                </div>
-                <div class="row">
-                  <div class="cell">
-                    <label>Price:</label>
-                  </div>
-                  <div class="cell">
-                   ${{ product.price.USD }}
-                  </div>
-                </div>
-                <div class="row">
-                  <div class="cell">
-                    <label>Quantity:</label>
-                  </div>
-                  <div class="cell">
-                    <input type="number" v-model.number="product.quantity">
-                  </div>
-                </div>
-              </form>
-            </div>
-            <div class="card-footer">
-              <button @click="addToCart(product.name, i)" class="btn btn-light">
-                Add to cart
-              </button>
-            </div>
-          </div>
+          <ProductCard 
+          v-for="(product, index) in inventory.slice(0,3)" 
+          :key="product.id" 
+          :index="index"
+          :product="product"
+          :addToCart="addToCart"
+          class="card">
+           
+          </ProductCard>
         </div>
       </main>
       <sidebar
@@ -69,12 +38,13 @@
 </template>
 
 <script>
-import food from '../food.json'
+import ProductCard from '@/components/ProductCard.vue'
 
 export default {
   name: 'Home',
-  props: ['inventory'],
+  props: ['inventory', 'addToCart'],
   components: {
+    ProductCard
   }
 }
 </script>
