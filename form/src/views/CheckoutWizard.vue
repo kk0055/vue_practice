1a4eb94a2c61ba8df56d5a9687c32425a9b082cd
kk0055 <rtsw.lgy@gmail.com>
2021-10-20 14:11:31 +0800

    PrimeVUe installed

--- form/src/views/CheckoutWizard.vue
+++ form/src/views/CheckoutWizard.vue
@@ -4,6 +4,8 @@
   <Contact/>
   <Shopping/>
   <Review/>
+
+   <Button label="Submit" />
   </div>
 </template>
 
@@ -12,14 +14,19 @@
 import Contact from '@/components/Contact.vue'
 import Shopping from '@/components/Shopping.vue'
 import Review from '@/components/Review.vue'
-
+import Button from 'primevue/button';
 export default {
   name: 'CheckoutWizard',
   components: {
+    Button,
     Contact,
     Shopping,
     Review
-    
+  },
+  data() {
+    return {
+      step:1
+    }
   }
 }
 </script>
