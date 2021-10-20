197d8401fac0d24b0c326a9cddf977d66f1119af
kk0055 <rtsw.lgy@gmail.com>
2021-10-20 12:21:15 +0800

    ファイルをいくつか作成

---
+++ form/src/components/Shopping.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="home">
+  <p>Shopping</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
---
+++ form/src/views/CheckoutWizard.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+  <p>Checkout</p>
+  <Contact/>
+  <Shopping/>
+  <Review/>
+  </div>
+</template>
+
+<script>
+
+import Contact from '@/components/Contact.vue'
+import Shopping from '@/components/Shopping.vue'
+import Review from '@/components/Review.vue'
+
+export default {
+  name: 'CheckoutWizard',
+  components: {
+    Contact,
+    Shopping,
+    Review
+    
+  }
+}
+</script>
