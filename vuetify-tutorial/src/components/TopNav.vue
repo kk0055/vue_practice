c28eeda2577d3ebb7ec013ec6af9d8ebbafe35c3
kk0055 <rtsw.lgy@gmail.com>
2022-01-31 22:10:27 +0900

    Router setting

---
+++ vuetify-tutorial/src/components/TopNav.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>TopNav</div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      drawer:false,
+      items: [
+        {title:"Home", link:'/', icon:'home'},
+        {title:"Store", link:'/store', icon:'shopping-basket'},
+        {title:"Cart", link:'/cart', icon:'shopping-cart'},
+      ]
+    }
+  }
+}
+</script>
--- vuetify-tutorial/src/router/index.js
+++ vuetify-tutorial/src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Store from '../views/Store.vue'
+import Cart from '../views/Cart.vue'
+import Checkout from '../views/Checkout.vue'
+import ThankYou from '../views/ThankYou.vue'
 
 Vue.use(VueRouter)
 
@@ -11,13 +15,25 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/Store',
+    name: 'Store',
+    component: Store
+  },
+  {
+    path: '/Cart',
+    name: 'Cart',
+    component: Cart
+  },
+  {
+    path: '/Checkout',
+    name: 'Checkout',
+    component: Checkout
+  },
+  {
+    path: '/ThankYou',
+    name: 'ThankYou',
+    component: ThankYou
+  },
 ]
 
 const router = new VueRouter({
