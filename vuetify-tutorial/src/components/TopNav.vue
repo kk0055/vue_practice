df1a9763ab05b1a1948111c8d1eab9f6a75b23fe
kk0055 <rtsw.lgy@gmail.com>
2022-01-31 22:56:05 +0900

    Navbar toggle

--- vuetify-tutorial/src/components/TopNav.vue
+++ vuetify-tutorial/src/components/TopNav.vue
@@ -1,8 +1,16 @@
 <template>
-  <v-app-bar>
+ <div>
+    <v-app-bar>
     <v-toolbar-title>Awesome Shop</v-toolbar-title>
+  
+  <v-spacer></v-spacer>
+  <span class="hidden-sm-and-up" >
+<v-btn @click.stop="drawer = !drawer" >
+  Menu
+</v-btn>
 
-    <v-toolbar-items v-for="item in items">
+  </span>
+    <v-toolbar-items class="hidden-xs-only" v-for="item in items" :key="item.title">
       <v-btn :to="item.link" text>
       <v-icon small left>{{item.icon}}</v-icon>
       {{ item.title }}
@@ -10,6 +18,17 @@
     
     </v-toolbar-items>
   </v-app-bar>
+
+<v-navigation-drawer 
+absolute
+right
+temporaly
+v-model="drawer"
+>
+  Hello
+</v-navigation-drawer>
+
+ </div>
 </template>
 <script>
 export default {
