0bd2bce4e91a5a819e49d5d35cdbc5bb2473ffa1
kk0055 <rtsw.lgy@gmail.com>
2022-02-02 00:21:57 +0900

    v-cardをコンポーネント化 props

---
+++ vuetify-tutorial/src/components/Cards/VerticalProduct.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-card outlined>
+    <v-img :src="product.image" height="200px"></v-img>
+    <v-card-title> {{ product.name }} </v-card-title>
+    <v-card-subtitle> ${{ product.price }} </v-card-subtitle>
+    <v-card-actions>
+      <v-btn color="success" outlined>
+        <v-icon>mdi-plus</v-icon>
+        Add to Cart
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    product:Object,
+  }
+}
+</script>
--- vuetify-tutorial/src/components/Home/PopularProducts.vue
+++ vuetify-tutorial/src/components/Home/PopularProducts.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container class="mb-12">
     <v-row>
-      <v-col sm="10" offset-sm="1" md="8" offset-md="2">
+      <v-col sm="10" offset-sm="1" lg="8" offset-lg="2">
         <h2>
           Popular Products
           <v-btn to="store" text color="primary" small>View All</v-btn>
@@ -10,15 +10,15 @@
     </v-row>
 
     <v-row>
-      <v-col sm="10" offset-sm="1" md="8" offset-md="2">
+      <v-col sm="10" offset-sm="1" lg="8" offset-lg="2">
         <v-row>
-          <v-col sm="6" md="4"
-          v-for="(product, index) in $store.state.products.slice(0,3)" :key="product.name"
-          > 
-          <v-card outlined>
-              <v-img :src="product.image" height="200px"></v-img>
-           <v-card-title> {{ product.name }} </v-card-title>
-          </v-card>
+          <v-col
+            sm="6"
+            md="4"
+            v-for="(product, index) in $store.state.products.slice(0, 3)"
+            :key="product.name"
+          >
+            <VerticalProduct :product="product" />
           </v-col>
         </v-row>
       </v-col>
@@ -27,13 +27,13 @@
 </template>
 
 <script>
-// import VerticalProduct from "../Cards/VerticalProduct";
+import VerticalProduct from "../Cards/VerticalProduct";
 export default {
   data() {
     return {};
   },
   components: {
-    // VerticalProduct,
+    VerticalProduct,
   },
 };
 </script>
