ba4f4da4e371ce784e7ef86cc5558068527b56a0
kk0055 <rtsw.lgy@gmail.com>
2022-02-02 23:45:15 +0900

    added Store SideBar

--- vuetify-tutorial/src/components/store/ProductDisplay.vue
+++ vuetify-tutorial/src/components/store/ProductDisplay.vue
@@ -2,30 +2,28 @@
   <div>
     <h3>Products</h3>
     <v-row>
-      <v-col sm="6" md="4"
-      v-for="(product,i) in products" :key="product.name"
-      >
-     <VerticalProduct :product="product" :i="i" :addToCart="addToCart" />
+      <v-col sm="6" md="4" v-for="(product, i) in products" :key="product.name">
+        <VerticalProduct :product="product" :i="i" :addToCart="addToCart" />
       </v-col>
     </v-row>
   </div>
 </template>
 <script>
-import { mapState, mapMutations } from 'vuex'
-import VerticalProduct from '@/components/Cards/VerticalProduct.vue'
+import { mapState, mapMutations } from "vuex";
+import VerticalProduct from "@/components/Cards/VerticalProduct.vue";
 export default {
   components: {
-    VerticalProduct 
+    VerticalProduct,
   },
   computed: {
-    ...mapState(['products'])
+    ...mapState(["products"]),
   },
   methods: {
-     ...mapMutations(['updateSnackbar', 'addItemToCart']),
-     addToCart(index, quantity = 1) {
-       this.addItemToCart({itemId:index, quantity})
-        this.updateSnackbar({ show: true })
-     }
-  }
-}
+    ...mapMutations(["updateSnackbar", "addItemToCart"]),
+    addToCart(index, quantity = 1) {
+      this.addItemToCart({ itemId: index, quantity });
+      this.updateSnackbar({ show: true });
+    },
+  },
+};
 </script>
