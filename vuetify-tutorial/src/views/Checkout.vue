ac4a2e38a3d6aff0f1ccddda66661b09d439e292
kk0055 <rtsw.lgy@gmail.com>
2022-02-03 23:34:16 +0900

    ContactInfo added

--- vuetify-tutorial/src/views/Checkout.vue
+++ vuetify-tutorial/src/views/Checkout.vue
@@ -14,6 +14,26 @@
 
             <v-stepper-step step="3"></v-stepper-step>
           </v-stepper-header>
+
+          <v-stepper-items>
+            <ContactInfo 
+              :data="data" 
+              :rules="rules" 
+              :next="next" />
+
+            <ShippingInfo
+              :data="data"
+              :rules="rules"
+              :next="next"
+              :previous="previous"
+            />
+
+            <Review
+              :data="data"
+              :submitOrder="submitOrder"
+              :previous="previous"
+            />
+          </v-stepper-items>
         </v-stepper>
       </v-col>
     </v-row>
@@ -42,7 +62,31 @@ export default {
         state: "",
         zip: "",
       },
+      rules: {
+        required: (value) => !!value || "Required.",
+        zip: (value) => value.length == 5 || "Must be five characters",
+        email: (value) => {
+          const pattern =
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+          return pattern.test(value) || "Invalid e-mail.";
+        },
+        phone: (value) => {
+          const pattern = /\d{10}/;
+          return pattern.test(value) || "Invalid phone number.";
+        },
+      },
     };
+  },
+  methods: {
+    next() {
+      this.step += 1;
+    },
+    previous() {
+      this.step -= 1;
+    },
+    submitOrder() {
+      this.$router.push({ name: "Thankyou" });
+    },
   },
 };
 </script>
