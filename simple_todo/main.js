cc25f79f063e0026851bab4ad6ffd4b25f7741b0
kk0055 <rtsw.lgy@gmail.com>
2022-02-08 23:21:30 +0900

    fetch remove  method added

--- simple_todo/main.js
+++ simple_todo/main.js
@@ -1,16 +1,16 @@
 var STORAGE_KEY = 'todos-vuejs-demo'
 var todoStorage = {
-  fetch: function() {
-    var todos = JSON.parse(
+  fetch() {
+    let todos = JSON.parse(
       localStorage.getItem(STORAGE_KEY) || '[]'
     )
-    todos.forEach(function(todo, index) {
+    todos.forEach( (todo, index) => {
       todo.id = index
     })
     todoStorage.uid = todos.length
     return todos
   },
-  save: function(todos) {
+  save: function (todos) {
     localStorage.setItem(STORAGE_KEY, JSON.stringify(todos))
   }
 }
@@ -18,20 +18,46 @@ var todoStorage = {
 const app = new Vue({
   el: '#app',
   data: {
-    todos: []
+    todos: [],
+    options: [
+      { value: -1, label: 'すべて' },
+      { value: 0,  label: '作業中' },
+      { value: 1,  label: '完了' }
+    ],
+    current : -1,
   },
   methods: {
     doAdd(event, value) {
-     let comment = this.$refs.comment
-     if (!comment.value.length) {
-       return
-     }
-     this.todos.push({
-       id: todoStorage.uid++,
-       comment:comment.value,
-       state: 0
-     })
-     comment.value = ''
+      let comment = this.$refs.comment
+      if (!comment.value.length) {
+        return
+      }
+      this.todos.push({
+        id: todoStorage.uid++,
+        comment: comment.value,
+        state: 0
+      })
+      comment.value = ''
+    },
+    doChangeState(item) {
+      item.state = item.state ? 0 : 1
+    },
+    doRemove(item) {
+      const index = this.todos.indexOf(item)
+      this.todos.splice(index, 1)
+    }
+  },
+  created() {
+      // インスタンス作成時に自動的に fetch() する
+   this.todos = todoStorage.fetch()
+  },
+  watch: {
+    todos: {
+      handler: function (todos) {
+        todoStorage.save(todos)
+      },
+      // deep オプションでネストしているデータも監視できる
+      deep: true
     }
   }
 })
